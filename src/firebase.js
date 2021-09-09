36d6ad6a1140819c5329e2437f3cc48263d34bb2
김하진 <knj9417@gmail.com>
2021-09-09 23:40:21 +0900

    React + Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGsI6tiNRACfeVI799j-exTsdOlMtWzDg",
+  authDomain: "hwitter-feda6.firebaseapp.com",
+  projectId: "hwitter-feda6",
+  storageBucket: "hwitter-feda6.appspot.com",
+  messagingSenderId: "389229513963",
+  appId: "1:389229513963:web:36eb80716d0ad679f702ae",
+};
+
+export default initializeApp(firebaseConfig);
